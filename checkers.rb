d3873a0912ddf4afe024f7796ea2d4710e2c066d
App Academy Student <student@appacademy.io>
2014-02-06 11:35:22 -0800

    Phase 1 done

--- checkers.rb
+++ checkers.rb
@@ -36,15 +36,100 @@ class Board
     end
   end
 
-  def perform_slide
+  def perform_slide(start_pos, end_pos)
+    possible_slides = self.slide_moves(start_pos)
+    return false if possible_slides.none? do |possible_pos|
+      end_pos == possible_pos
+    end
+
+    # Performing slide
+    piece_to_move = @grid[start_pos.first][start_pos.last]
+    @grid[start_pos.first][start_pos.last] = nil
+    @grid[end_pos.first][end_pos.last] = piece_to_move
+
+    self.maybe_promote(end_pos)
+    true
   end
 
-  def perform_jump
+  def perform_jump(start_pos, end_pos)
+    possible_jumps = self.jump_moves(start_pos)
+    target_pos = nil
+    return false if possible_jumps.none? do |jump_pos, enemy_pos|
+      if end_pos == jump_pos
+        target_pos = enemy_pos
+        true
+      end
+    end
+
+    # Performing jump
+    piece_to_move = @grid[start_pos.first][start_pos.last]
+    @grid[start_pos.first][start_pos.last] = nil
+    @grid[end_pos.first][end_pos.last] = piece_to_move
+
+    # Delete the target piece
+    @grid[target_pos.first][target_pos.last] = nil
+
+    self.maybe_promote(end_pos)
+    true
   end
 
-  def move_diffs(pos)
+  def maybe_promote(piece_pos)
+    # Get the piece
+    piece = self.get_piece(piece_pos)
+    # Black piece has reach the end of the board and needs to be promoted
+    if piece.color == :black && piece_pos.first == 7
+      piece.is_king = true
+    elsif piece.color == :white && piece_pos.first == 0
+      piece.is_king = true
+    end
+  end
+
+  def get_piece(pos)
+    @grid[pos.first][pos.last]
+  end
+
+  def jump_moves(pos)
     piece = @grid[pos.first][pos.last]
     raise StandardError.new("No piece found!") if piece.nil?
+
+    # Generate diffs for jumps
+    jumps = nil
+    target_pos = nil
+    if piece.is_king
+      moves      =  [[-2, -2], [-2, 2], [2, -2], [2, 2]]
+      target_pos =  [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+    elsif piece.color == :black
+      moves = [[2, -2], [2, 2]]
+      target_pos = [[1, -1], [1, 1]]
+    else
+      moves = [[-2, -2], [-2, 2]]
+      target_pos = [[-1, -1], [-1, 1]]
+    end
+
+    # Translate diffs into actual positions
+    moves.map! do |d_row, d_col|
+      [pos.first + d_row, pos.last + d_col]
+    end
+    target_pos.map! do |d_row, d_col|
+      [pos.first + d_row, pos.last + d_col]
+    end
+
+    # Zip jumps and target_pos into pairs
+    jump_target_pairs = moves.zip(target_pos)
+    # p jump_target_pairs
+
+    # Filter out out possible moves
+    jump_target_pairs.reject do |jump_pos, target_pos|
+      target_piece = self.get_piece(target_pos)
+      # Reject if jump is out of bound, target position is nempty or same color
+      !self.in_bound?(jump_pos) || target_piece.nil? || target_piece.color == piece.color
+    end
+  end
+
+  def slide_moves(pos)
+    piece = @grid[pos.first][pos.last]
+    raise StandardError.new("No piece found!") if piece.nil?
+
     moves = nil
     if piece.is_king
       moves = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
@@ -53,10 +138,15 @@ class Board
     else
       moves = [[-1, -1], [-1, 1]]
     end
+
+    # Translate diffs into actual positions
     moves.map do |d_row, d_col|
       move = [pos.first + d_row, pos.last + d_col]
-    end.select do |move|
-      self.in_bound?(move)
+    end.select do |pos|
+      self.in_bound?(pos)
+    end.select do |pos|
+      # Check if position is empty
+      @grid[pos.first][pos.last].nil?
     end
   end
 
@@ -95,4 +185,14 @@ class Checkers
   end
 end
 
-Board.new.render
+g = Board.new
+g.render
+# p g.slide_moves([2, 1])
+g.perform_slide([2, 1], [3, 2])
+g.render
+# p g.slide_moves([3, 2])
+g.perform_slide([3, 2], [4, 1])
+g.render
+# p g.jump_moves([5, 0])
+g.perform_jump([5, 0], [3, 2])
+g.render
